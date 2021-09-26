8e26f00c94106595f27ac9af27789217f679f5ad
Pradipta Das <76657986+pradiptaDas97@users.noreply.github.com>
2021-09-26 19:26:22 +0530

    Add files via upload

---
+++ mean-stack-crud-app/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { EmployeeCreateComponent } from './components/employee-create/employee-create.component';
+import { EmployeeEditComponent } from './components/employee-edit/employee-edit.component';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+
+const routes: Routes=[
+  { path:'',pathMatch:'full', redirectTo:'create-employee'},
+  { path:'create-employee',component:EmployeeCreateComponent},
+  { path:'edit-employee/:id',component:EmployeeEditComponent},
+  { path:'employee-list',component:EmployeeListComponent},
+]
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
